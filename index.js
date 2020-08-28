c64a07537511b7a9b6a98fc8460f4a769b7c3bbf
ProstoOleg201 <prosto.oleg201@ya.ru>
2020-08-28 19:57:22 +0300

    second commit

--- index.js
+++
@@ -1,9 +0,0 @@
-const TelegramBot = require('node-telegram-bot-api')
-
-const TOKEN = '1185029109:AAGxJlPbIlj2VxIzNb_KFgT6uhBx1IdNvec'
-
-const bot = new TelegramBot(TOKEN, {polling: true})
-
-bot.on('message',msg => {
- bot.sendMessage(msg.chat.id, `Hello from Me, bot says : "Hi, ${msg.from.first_name}"`)
-})
