a1cffe593797a8ed7c22151c4c20fbe027974809
ProstoOleg201 <prosto.oleg201@ya.ru>
2020-08-31 23:37:48 +0300

    Add new bot.on

--- index.js
+++ index.js
@@ -1,9 +1,27 @@
 const TelegramBot = require('node-telegram-bot-api')
-
 const TOKEN = '1185029109:AAGxJlPbIlj2VxIzNb_KFgT6uhBx1IdNvec'
 
 const bot = new TelegramBot(TOKEN, {polling: true})
 
-bot.on('message',msg => {
+/* bot.on('message',msg => {
     bot.sendMessage(msg.chat.id, `Hello: "Hi, ${msg.from.first_name}"`)
-})
+}
+ */
+
+bot.on('text', function (msg)
+{
+    let messageChatId = msg.chat_id;
+    let messageText = msg.text;
+    let messageDate = msg.date;
+    let messageUsr = msg.from.username;
+
+    sendMessageByBot(messageChatId, messageText, messageDate, messageUsr);
+
+    console.log(msg);
+});
+
+function sendMessageByBot (aId, aText, aDate, aName)
+{
+    let text = 'I\'m a cute bot!';
+    bot.sendMessage(aId, aText, aDate, aName);
+}
