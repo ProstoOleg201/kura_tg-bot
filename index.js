60e0a34930567d3493312d34eb19f68d2a087d75
ProstoOleg201 <prosto.oleg201@ya.ru>
2020-09-01 00:39:11 +0300

    Add new bot.on (4)

--- index.js
+++ index.js
@@ -1,7 +1,15 @@
-const TelegramBot = require('node-telegram-bot-api')
-const token = '1185029109:AAGxJlPbIlj2VxIzNb_KFgT6uhBx1IdNvec'
+// Подключаем библиотеку для работы с Telegram API в переменную
+var TelegramBot = require('node-telegram-bot-api');
 
-bot.on('message', (msg) => {
-    const chatId = msg.chat.id;
-    bot.sendMessage(chatId, 'Получили твое сообщение! Спасибо!');
+// Устанавливаем токен, который выдавал нам бот
+var token = '1185029109:AAGxJlPbIlj2VxIzNb_KFgT6uhBx1IdNvec';
+// Включить опрос сервера. Бот должен обращаться к серверу Telegram, чтобы получать актуальную информацию
+// Подробнее: https://core.telegram.org/bots/api#getupdates
+var bot = new TelegramBot(token, { polling: true });
+
+// Написать мне ... (/echo Hello World! - пришлет сообщение с этим приветствием, то есть "Hello World!")
+bot.onText(/\/echo (.+)/, function (msg, match) {
+    var fromId = msg.from.id; // Получаем ID отправителя
+    var resp = match[1]; // Получаем текст после /echo
+    bot.sendMessage(fromId, resp);
 });
