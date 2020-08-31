b1dd0342f0d758b6b59c9904b4c79067aaf4df15
ProstoOleg201 <prosto.oleg201@ya.ru>
2020-09-01 00:36:50 +0300

    Add new bot.on (4)

--- index.js
+++ index.js
@@ -1,10 +1,7 @@
 const TelegramBot = require('node-telegram-bot-api')
-const TOKEN = '1185029109:AAGxJlPbIlj2VxIzNb_KFgT6uhBx1IdNvec'
-
-const bot = new TelegramBot(TOKEN, {polling: true})
+const token = '1185029109:AAGxJlPbIlj2VxIzNb_KFgT6uhBx1IdNvec'
 
 bot.on('message', (msg) => {
     const chatId = msg.chat.id;
-
     bot.sendMessage(chatId, 'Получили твое сообщение! Спасибо!');
 });
