9ba4f62ec30a7a2753ca02c7cfa9917fdaf28318
ProstoOleg201 <prosto.oleg201@ya.ru>
2020-09-01 00:52:25 +0300

    Mew Token

--- index.js
+++ index.js
@@ -1,15 +1,15 @@
 // Подключаем библиотеку для работы с Telegram API в переменную
-var TelegramBot = require('node-telegram-bot-api');
+let TelegramBot = require('node-telegram-bot-api');
 
 // Устанавливаем токен, который выдавал нам бот
-var token = '1185029109:AAEMIODQ0Cee82UeF81WJ5Ze6vUPgI8cuos';
+let token = '1185029109:AAEMIODQ0Cee82UeF81WJ5Ze6vUPgI8cuos';
 // Включить опрос сервера. Бот должен обращаться к серверу Telegram, чтобы получать актуальную информацию
 // Подробнее: https://core.telegram.org/bots/api#getupdates
-var bot = new TelegramBot(token, { polling: true });
+let bot = new TelegramBot(token, { polling: true });
 
 // Написать мне ... (/echo Hello World! - пришлет сообщение с этим приветствием, то есть "Hello World!")
-bot.onText(/\/echo (.+)/, function (msg, match) {
-    var fromId = msg.from.id; // Получаем ID отправителя
-    var resp = match[1]; // Получаем текст после /echo
-    bot.sendMessage(fromId, resp);
+bot.on('message', function (msg) {
+    let fromId = msg.from.id; // Получаем ID отправителя
+    let myText = msg.text; // Получаем текст после /echo
+    bot.sendMessage(fromId, myText);
 });
